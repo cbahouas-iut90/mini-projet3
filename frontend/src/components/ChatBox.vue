38b62ab9a288e9bd3c05700f55ae80905f0457c9
cbahouas <christ.bahouassila.edu@univ-fcomte.fr>
2025-05-15 16:56:33 +0200

    ajout du frontend

---
+++ frontend/src/components/ChatBox.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="msg in messages" :key="msg._id">
+        <strong>{{ msg.sender?.name }}:</strong> {{ msg.text }}
+      </li>
+    </ul>
+    <input v-model="newMessage" @keyup.enter="sendMessage" placeholder="Votre message..." />
+  </div>
+</template>
+
+<script>
+import io from 'socket.io-client';
+const socket = io('http://localhost:3000', { withCredentials: true });
+
+export default {
+  name: 'ChatView',
+  data() {
+    return {
+      messages: [],
+      newMessage: ''
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/api/messages", { credentials: 'include' })
+        .then(res => res.json())
+        .then(data => this.messages = data);
+
+    socket.on("message", (msg) => {
+      this.messages.push(msg);
+    });
+  },
+  methods: {
+    sendMessage() {
+      if (this.newMessage.trim()) {
+        socket.emit("message", this.newMessage);
+        this.newMessage = "";
+      }
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
---
+++ frontend/src/components/UserLogin.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Connexion</h1>
+    <a :href="googleAuthUrl">
+      <button>Connexion avec Google</button>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginView',
+  computed: {
+    googleAuthUrl() {
+      return 'http://localhost:3000/auth/google';
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
